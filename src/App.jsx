132142db715693daf4e9300fe314ff677459cb9c
nmeachum7883 <nmeachum7883@gmail.com>
2026-05-08 15:40:56 -0400

    Create: start up files and templates for portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return <div className="container mx-auto max-w-7xl">
+    {/* Navbar */}
+    {/* Hero */}
+    {/* About */}
+    {/* Projects */}
+    {/* Experience */}
+    {/* Testimonials OR related section */}
+    {/* Contact */}
+    {/* Footer */}
+  </div>;
+};
+
+export default App;
