34d449930bf482ceb2909a1cfa9b5e0a17193514
nmeachum7883 <nmeachum7883@gmail.com>
2026-05-10 19:00:19 -0400

    Create: Navbar component for landingPage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return <div className="container mx-auto max-w-7xl">
-    {/* Navbar */}
+    <Navbar />
     {/* Hero */}
     {/* About */}
     {/* Projects */}
